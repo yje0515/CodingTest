4c9de03b81761a0753eef3908afc42f7b224282b
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-10-09 02:42:24 +0900

    [level 1] Title: 콜라 문제, Time: 0.09 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -3,13 +3,13 @@ class Solution {
         int totalBottles = n;
         int returnBottles = 0;
         while(totalBottles>=a){
-            //교환한 횟수
+            //교환한 횟수 = 보유중인 병/a
             int exchanged = totalBottles/a;
-            //교환하고 남은 병
+            //교환하고 남은 병 = 보유중인 병/a나머지
             int remainder = totalBottles%a;
-            //교환된 병
+            //교환된 병 = 교환한 횟수 * b
             returnBottles += exchanged*b;
-            //현재 있는 병
+            //현재 있는 병 = 교환된 병 + 교환하고 남은 병
             totalBottles = exchanged*b+remainder;
         }
         return returnBottles;
