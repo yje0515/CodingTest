689acbf2514dbfb09b4b794dc4b90d117ff6bd4a
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-10-12 03:57:11 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,6 +1,8 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        // k%numbers.length  k번째로 공을 던지는 사람의 번호
+        // 1명씩 건너뜀 2*k
+        // k번째로 공을 "던지는" 사람의 번호(-2)
+        // %numbers.length
         return numbers[(2*k-2)%numbers.length];
     }
 }
