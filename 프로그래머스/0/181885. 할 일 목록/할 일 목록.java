a31ac8a37a922d44425a1ba556d92cb138eca81e
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-10-11 16:59:35 +0900

    [level 0] Title: 할 일 목록, Time: 12.91 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -3,11 +3,7 @@ class Solution {
         String answer = "";
         for(int i=0;i<todo_list.length;i++){
             if(!finished[i]){
-                if(i<todo_list.length-1){
                     answer += todo_list[i]+",";
-                }else{
-                    answer += todo_list[i];
-                }
             }
         }
         return answer.split(",");
