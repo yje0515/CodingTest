92947d9039777cb7c0c54bd13d79a18139cccc9b
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-10-17 01:59:51 +0900

    [level 0] Title: 2의 영역, Time: 2.29 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        int start = -1;
+        int end = -1;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==2){
+                if(start==-1){
+                    start = i;
+                }
+                end = i;
+            }
+        }
+        if(start ==-1){
+            return new int[] {-1};
+        }
+        return Arrays.copyOfRange(arr,start,end+1);
+    }
+}
