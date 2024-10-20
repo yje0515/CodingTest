a73959a8510e26892b9272688251ed3f6c9d25d1
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-10-20 22:18:50 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -1,13 +1,11 @@
 class Solution {
     public int solution(int order) {
-        int[] arr = new int[(int)Math.log10(order)+1];
         int count = 0;
-        for(int i=0; i<arr.length;i++){
-            arr[i] = order%10;
-            order = order/10;
-            if(arr[i]==3||arr[i]==6||arr[i]==9){
+        while(order>0){
+            if(order%10==3||order%10==6||order%10==9){
                 count++;
             }
+            order = order/10;
         }
         return count;
     }
