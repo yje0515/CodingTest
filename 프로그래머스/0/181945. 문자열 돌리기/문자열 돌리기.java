de2ab4ca26dfde8bafa575b963fdb8da9bcf4b16
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-10-21 01:35:48 +0900

    [level 0] Title: 문자열 돌리기, Time: 151.73 ms, Memory: 63.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String[] arr = a.split("");
+        for(String b : arr){
+           System.out.println(b); 
+        }
+        
+    }
+}
