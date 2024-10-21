6d3625ced16cda1330659193d833beced0f92e02
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-10-21 18:38:36 +0900

    [level 0] Title: 세로 읽기, Time: 16.17 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -1,9 +1,8 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
-        String[] arr = my_string.split("");
         String answer = "";
-        for(int i=c-1; i<arr.length;i+=m){
-            answer+=arr[i];
+        for(int i=c-1; i<my_string.length();i+=m){
+            answer+=my_string.charAt(i);
         }
         return answer;
     }
