572474adf088610f7198c45769f06ae202876275
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-10-24 02:46:51 +0900

    [level 0] Title: 문자열 뒤집기, Time: 15.02 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,13 +1,7 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
-        StringBuilder answer = new StringBuilder();
-        answer.append(my_string.substring(0,s));
-        
         StringBuilder reversed = new StringBuilder(my_string.substring(s,e+1));
-        answer.append(reversed.reverse());
-        
-        answer.append(my_string.substring(e+1,my_string.length()));
-        
-        return answer.toString();
+        reversed.reverse();
+        return my_string.substring(0,s)+reversed.toString()+my_string.substring(e+1);
     }
 }
