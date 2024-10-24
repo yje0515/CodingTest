4383aa808e3c59aef63c290bcb84ff861769f499
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-10-24 15:15:33 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int target = Integer.bitCount(n);
+        int target = Integer.bitCount(n);//2진수의 1의 갯수를 세는 메서드
         int answer = n;
         while(true){
             answer++;
