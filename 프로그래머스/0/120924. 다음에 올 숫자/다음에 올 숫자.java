0148685c0aa26832c916719adac399c3143682f9
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-10-28 23:01:58 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,15 +1,10 @@
 class Solution {
     public int solution(int[] common) {
-        // 공차 계산
-        int diff = common[1] - common[0];
-        
-        // 등차수열 확인
-        if (common[2] - common[1] == diff) {
-            return common[common.length - 1] + diff; // 다음 항
+        int a = common[1]-common[0];
+        if(common[2]-common[1]==a){
+            return common[common.length-1]+a;
         }
-
-        // 등비수열 확인
-        int ratio = common[1] / common[0];
-        return common[common.length - 1] * ratio; // 다음 항
+        int b = common[1]/common[0];
+        return common[common.length-1]*b;
     }
 }
