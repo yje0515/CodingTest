8d3789bb2b8833442209e93828e4c59868867140
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-10-29 13:23:05 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 17.81 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -2,7 +2,6 @@ class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         return my_string.substring(0,s)
             +overwrite_string
-            +my_string.substring(overwrite_string.length()+s,
-                                my_string.length());
+            +my_string.substring(overwrite_string.length()+s);
     }
 }
