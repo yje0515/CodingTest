d2a507602c1949ca5a996816b33c738b919676ff
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-10-31 17:37:40 +0900

    [level 0] Title: k의 개수, Time: 3.54 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,16 +1,15 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        int count=0;
-        char target = (char) (k+'0');
+        int answer = 0;
         for(int index=i;index<=j;index++){
-            String str = Integer.toString(index);
-            for(int z=0;z<str.length();z++){
-                char c = str.charAt(z);
-                if(c==target){
-                    count++;
+            int tmp = index;
+            while(tmp!=0){
+                if(tmp%10==k){
+                    answer++;
                 }
+                tmp/=10;
             }
         }
-        return count;
+        return answer;
     }
 }
