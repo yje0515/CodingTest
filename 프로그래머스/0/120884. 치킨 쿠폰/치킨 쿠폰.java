62554c83c9b6959404997813a016e7ef6f1a8346
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-11-07 01:33:44 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.04 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int chicken) {
         int answer = 0;
-        int coupon = chicken;
+        int coupon = chicken;//초기 쿠폰
         while(coupon>=10){
-            int newChicken = coupon/10;
+            int newChicken = coupon/10;//새로 받은 치킨
             coupon = newChicken + (coupon%10);
             answer += newChicken;
         }
