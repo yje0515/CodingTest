e6715b1679ff7fe020f02c0bcfb02cdf36f4a096
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-11-09 02:05:40 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[] solution(String[] keyinput, int[] board) {
         int[] answer = new int[2];
-        int xLimit = board[0]/2;
-        int yLimit = board[1]/2;
-        for(String a:keyinput){
-            if(a.equals("left")){
+        int xLimit = board[0] / 2;
+        int yLimit = board[1] / 2;
+        
+        for (String direction : keyinput) {
+            if (direction.equals("left") && answer[0] > -xLimit) {
                 answer[0]--;
-            }else if(a.equals("right")){
+            } else if (direction.equals("right") && answer[0] < xLimit) {
                 answer[0]++;
-            }else if(a.equals("up")){
+            } else if (direction.equals("up") && answer[1] < yLimit) {
                 answer[1]++;
-            }else if(a.equals("down")){
+            } else if (direction.equals("down") && answer[1] > -yLimit) {
                 answer[1]--;
             }
-            answer[0] = Math.max(-xLimit,Math.min(xLimit,answer[0]));
-            answer[1] = Math.max(-yLimit,Math.min(yLimit,answer[1]));
         }
+        
         return answer;
     }
 }
